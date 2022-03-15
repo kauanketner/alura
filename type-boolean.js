f04bdb8cea9c3957a7d20c9661432f36702f056b
Kauan Ketner <kauanketner@icloud.com>
2022-03-15 20:25:42 -0300

    first commit

---
+++ type-boolean.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 10;
+const segundoNumero = 50;
+let cadastroAtivado = false;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = alura;
+const texto2 = kauan;
+
+console.log(texto1 === texto2);
