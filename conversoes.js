44821026bdfc015d79758f6d6857cba629e93532
Kauan Ketner <kauanketner@icloud.com>
2022-03-22 16:21:43 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,4 @@
+const numero = 456;
+const numberString = Number("456");
+
+console.log(numero + numberString)
---
+++ type-string.js
@@ -0,0 +1,11 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'Olá mundo';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Kauan"
+
+//concatenação (+) 
+
+console.log(citacao + meuNome);
